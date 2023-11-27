43e69c2cfa97fc6d9886398046d19d1e5e8d15d5
thomasIRA <thomas.al@live.ca>
2023-11-27 11:03:15 -0500

    model using eloquent

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogPost>
+ */
+class BlogPostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'body' => fake()->paragraph(30),
+            'user_id' => User::factory()
+        ];
+    }
+}
+
